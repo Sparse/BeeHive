47bac4b701783dcfc06f9f3e0d8b61a5e9435df6
Sparse <albert.christopher@hotmail.com>
2013-12-22 23:56:10 -0500

    Noticed Bug

--- QueenBee.cs
+++ QueenBee.cs
@@ -35,6 +35,7 @@ namespace BeeHive
 
             for (int i = 0; i < mWorkerBee.Length; i++)
             {
+                //TODO: fix bug with mWorkerBee.ShiftsLeft property, as I did not do it properly (infact, I didn't even assign to it correctly)
                 if (mWorkerBee[i].WorkShift()) workReport += "Worker Bee " + (i + 1).ToString() + " finished the task " + mWorkerBee[i].CurrentJob.ToString() + "\r\n";
                 if (string.IsNullOrEmpty(mWorkerBee[i].CurrentJob)) workReport += "Worker Bee " + (i + 1).ToString() + " isn't working! \r\n";
                 else
